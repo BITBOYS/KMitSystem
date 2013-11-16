31f270ba51ff9755006c845033d806fae98e9110
Maik <>
2013-11-16 18:16:40 +0100

    Neuer Queri: alle turnier zu einem team holen

--- KMitSystem/src/java/com/kmitsystem/tools/database/queries/DBTeamTournamentQueries.java
+++ KMitSystem/src/java/com/kmitsystem/tools/database/queries/DBTeamTournamentQueries.java
@@ -1,10 +1,16 @@
 package com.kmitsystem.tools.database.queries;
 
+import com.kmitsystem.tools.database.DatabaseHandler;
+import com.kmitsystem.tools.errorhandling.ErrorHandler;
+import com.kmitsystem.tools.errorhandling.Errors;
 import com.kmitsystem.tools.objects.Tournament;
+import com.kmitsystem.tools.objects.User;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 /**
@@ -16,27 +22,29 @@ public class DBTeamTournamentQueries {
     private static Connection con = null;
     private static ResultSet resultSet = null;
     
-    public static List<Tournament> getAllTournamentsFromTeam(String teamname) {
+    public static List<Tournament> getAllTournamentsFromTeam(String name) {
         List<Tournament> tournaments = new ArrayList<Tournament>();
             
-//        try {
-//            con = DatabaseHandler.connect();
-//            statement = con.createStatement();
-//            
-//            statement.executeQuery("SELECT username, email, password, goals,"
-//                                            + "goals_conceded, wins, defeats"
-//                                 + " FROM  user, user_team"
-//                                 + " WHERE team = " + teamname
-//                                 + "   AND username = user" );
-//            
-//            while(!resultSet.isAfterLast()) {
-//                tournaments.add(new Tournament(resultSet.getString("name"), 
-//                                               resultSet.getString(teamname)null), teamname, null, null, null, nr_matchdays));
-//                resultSet.next();
-//            }
-//        } catch (SQLException ex) {
-//            ErrorHandler.handle(Errors.DB_CONNECTION_ERROR, ex.getSQLState() + " " +ex.getMessage());
-//        }
+        try {
+            con = DatabaseHandler.connect();
+            statement = con.createStatement();
+            
+            resultSet = statement.executeQuery("SELECT name, leader, start_date, end_date, password , "
+                                                     + "nr_of_matchdays, venue, term_of_application" +
+                                              " FROM tournament, team_tournament" +
+                                              " WHERE team =  '" + name + "'" +
+                                              " AND tournament = name");
+            resultSet.first();
+            
+            while(!resultSet.isAfterLast()) {
+                tournaments.add(new Tournament(resultSet.getString("name"),     resultSet.getString("password"), new User(resultSet.getString("leader")), 
+                                               resultSet.getDate("start_date"), resultSet.getDate("end_date"),   resultSet.getInt("nr_of_matchdays"), 
+                                               resultSet.getString("venue"),    resultSet.getDate("term_of_application")));
+                resultSet.next();
+            }
+        } catch (SQLException ex) {
+            ErrorHandler.handle(Errors.DB_ERROR, ex.getSQLState() + " " +ex.getMessage());
+        }
         
         return tournaments;
     }
