86522d130ad21f465e370b2a3977375760ad41b9
Smadback <Smadback@192.168.1.13>
2013-11-17 12:27:05 +0100

    Debug getter-setter methoden

--- KMitSystem/src/java/com/kmitsystem/tools/objects/Tournament.java
+++ KMitSystem/src/java/com/kmitsystem/tools/objects/Tournament.java
@@ -66,15 +66,15 @@ public class Tournament {
         return start_date;
     }
     
-    public void getStart_date(Date start_date) {
+    public void setStart_date(Date start_date) {
         this.start_date = start_date;
     }
 
-    public void setEnd_date(Date end_date) {
-        this.end_date = end_date;
+    public Date getEnd_date() {
+        return end_date;
     }
 
-    public void getEnd_date(Date end_date) {
+    public void setEnd_date(Date end_date) {
         this.end_date = end_date;
     }
 
