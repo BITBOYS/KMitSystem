35921fad79c8f14e25b8733b80e427e8c50609b6
malte005 <malte005@yahoo.de>
2013-11-26 13:30:36 +0100

    Update DatenbankFüllen.sql

--- Datenbank/DatenbankFüllen.sql
+++ Datenbank/DatenbankFüllen.sql
@@ -10,8 +10,8 @@ INSERT INTO user_team (user, team, enter_dat)
 		  VALUES ("Maik", "Team Dede", "2013-11-10");
 
 INSERT INTO tournament (name, leader, start_date, end_date, 
-					    nr_of_matchdays, venue, term_of_application)
-		  VALUES ("Tournament Xa", "Maik", "2013-12-24", "2014-01-01", 4, "Kicko Geheimo", "2013-12-20");
+					    nr_of_matchdays, venue, term_of_application, description)
+		  VALUES ("Tournament Xa", "Maik", "2013-12-24", "2014-01-01", 4, "Kicko Geheimo", "2013-12-20", "Beschreibung Beschreibung Beschreibung Beschreibung Beschreibung Beschreibung Beschreibung Beschreibung Beschreibung Beschreibung");
 
 INSERT INTO team_tournament (team, tournament, goals, goals_conceded, 
 					wins, defeats) 
